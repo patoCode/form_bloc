acd462c197e6d0952397a4538eb88ff2ef0c17ff
Denis Rodriguez <denis_design@live.cl>
2023-12-26 10:38:57 -0400

    REFACTOR HANDLER DE EVENTOS terminado

--- lib/presentation/blocs/counter_bloc/counter_bloc.dart
+++ lib/presentation/blocs/counter_bloc/counter_bloc.dart
@@ -11,10 +11,15 @@ part 'counter_state.dart';
 class CounterBloc extends Bloc<CounterEvent, CounterState> {
   CounterBloc() : super(const CounterState()) {
     // * handler o manejador de los eventos
-    on<CounterIncresed>((event, emit) {
-      emit(state.copyWith(
-          counter: state.counter + event.values,
-          transactionCount: state.transactionCount + 1));
-    });
+    // * La linea de abajo es lo mismo q hacer on<CounterIncresed>((event, emit) => _onCounterEncreased(event, emit))
+    on<CounterIncresed>(_onCounterEncreased);
+    // * EXPLICACION => hacer lo de arriba solo funciona cuando la primera funcion enviara los mismos parametros en el mismo orden en el que los recibe a la otra funcion
+    // * ["on" enviara los mismos parametros en el mismo orden a la funcion "_onCounterEncrease"]
+  }
+
+  void _onCounterEncreased(CounterIncresed event, Emitter<CounterState> emit) {
+    emit(state.copyWith(
+        counter: state.counter + event.values,
+        transactionCount: state.transactionCount + 1));
   }
 }
